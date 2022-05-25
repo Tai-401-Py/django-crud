ad51ce8fb3faaa78052528c6ee6545997e0867d4
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-05-25 10:54:49 -0700

    created url routing for CRUD functions

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import SnackListView, SnackDetailView
+from .views import SnackCreateView, SnackDeleteView, SnackListView, SnackDetailView, SnackUpdateView
 
 urlpatterns = [
     path('', SnackListView.as_view(), name='snacks'),
-    path('<int:pk>/', SnackDetailView.as_view(), name='snack_detail')
+    path('<int:pk>/', SnackDetailView.as_view(), name='snack_detail'),
+    path('create/', SnackCreateView.as_view(), name='snack_create'),
+    path('<int:pk>/update/', SnackUpdateView.as_view(), name='snack_update'),
+    path('<int:pk>/delete/', SnackDeleteView.as_view(), name='snack_delete'),
 ]
